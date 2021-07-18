758c4d1777d18aaac94569acfc01a99eaed35b62
william marinho dos santos <56370582+williammarinho93@users.noreply.github.com>
2021-07-18 19:08:31 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { Storage } from "./src/data/Storage.js";
+import { cutURL } from "./src/cutURL.js";
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+if (Storage.data.links === undefined) {
+    Storage.data.links = [];
+    Storage.write();
+  }
+
+
+app.post("/cut", cutURL);
+app.get("/[a-f0-9]{5}" , cutURL);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
